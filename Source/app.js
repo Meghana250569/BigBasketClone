28313ed6f1e6bb045f0445d1a796b6b503f8af1b
Meghana <meghanajsmeghana@gmail.com>
2026-06-23 23:15:34 +0530

    chore:updated verson of app.js file

--- Source/app.js
+++ Source/app.js
@@ -6,11 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-);
+app.use( express.urlencoded({extended: true}));
 
 app.use(morgan("dev"))
 
@@ -21,35 +17,44 @@ app.get("/", (req, res) => {
     });
 });
 
+ app.use(
+     "/api/auth",
+     require("./routes/authRoutes")
+)
+
+ app.use(
+    "/api/user",
+    require("./routes/userRoutes")
+ )
+
+ app.use(
+     "/api/category",
+     require("./routes/categoryRoutes")
+ );
+
+ app.use(
+     "/api/subcategories",
+     require("./routes/subCategoryRoutes")
+ );
+
+ app.use(
+     "/api/products",
+     require("./routes/productRoutes")
+ );
 
 
-// app.use(
-//     "/api/category",
-//     require("./routes/categoryRoutes")
-// );
-
-// app.use(
-//     "/api/subcategories",
-//     require("./routes/subCategoryRoutes")
-// );
-
-// app.use(
-//     "/api/products",
-//     require("./routes/productRoutes")
-// );
-
-
-// app.use((req, res) => {
-//     res.status(404).json({
-//         success: false,
-//         message: "Route not found"
-//     });
-// });
+ app.use((req, res) => {
+     res.status(404).json({
+         success: false,
+         message: "Route not found"
+     });
+ });
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
 
-    res.status(err.statusCode(500)).json({
+    const statusCode=err.statusCode || 500
+    res.status(statusCode).json({
         success: false,
         message:
             err.message || "Internal Server Error"
