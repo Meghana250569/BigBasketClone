30351b0823842018b9ff9f40a8edc814696c82b0
Meghana <meghanajsmeghana@gmail.com>
2026-07-24 14:52:04 +0530

    chore:Addinf otpGerate.js file

---
+++ Source/utils/otpGenerate.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+
+console.log(process.env.EMAIL_USER)
+console.log(process.env.EMAIL_PASS)
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+const sendOtp = async (email, otp) => {
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Your OTP Code",
+    text: `Your OTP is ${otp}`
+  });
+};
+
+module.exports = sendOtp;
